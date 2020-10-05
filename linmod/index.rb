f543576d7deac0f4d0d131318967ffa833577acc
covid1974 <arnabc74@gmail.com>
2020-10-05 07:03:11 +0530

    silly

--- linmod/index.rb
+++ linmod/index.rb
@@ -1,12 +1,35 @@
 <NOTE>
 @{<E>
-<UPDT>MON SEP 21 IST 2020</UPDT>
+<UPDT>MON OCT 05 IST 2020</UPDT>
 <HEAD1>Linear Statistical Models</HEAD1>
 This is the webpage for the Linear Models course (B. Stat. (hons)
 III, 2020).
 <P/>
 <NEWS src="news.html"/>
+
 <P/>
+
+<HEAD2>Video lectures and exercises</HEAD2>
+<UL>
+<LI><LINK to="https://drive.google.com/file/d/1Au2-iI3t0JFdIpuz1UThwJgAWgAjV6sI/view?usp=sharing">Lecture 1</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1snIfrV_ouAqCDIZZqY3V2aUCCWm736SE/view?usp=sharing">Lecture 2</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1LD2SvP2j7IkoI8_jIFRrGdDUrGT1HsMq/view?usp=sharing">Lecture 3</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1NBr-h5Vnxmp3T0FFGX4A7skjwVqf9z9b/view?usp=sharing">Lecture 4</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1b8M-_9T2IEQbqp-r2I0e6-Co4pBmchVY/view?usp=sharing">Lecture 5</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1LGJ8www6y0RZl-bJR2Ao6xnEERb79ke8/view?usp=sharing">Lecture 6</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1rUBOTG5bpGdaP_YjYzZKi-HuWALa50T7/view?usp=sharing">Lecture 7</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1huAsgDbx2XNJ_cnph3fTYRBPWdu2F9Vf/view?usp=sharing">Lecture 8</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1tqdaPtU-APo5wlF0llJKSvvWa5-f374E/view?usp=sharing">Lecture 9</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1soHe1WQJFFXkKl1GJxalDimzjhVuWjnh/view?usp=sharing">Lecture 10</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/1iC2wztpZCX2czDks0HDeCe5I-rgbNkWo/view?usp=sharing">Lecture 11</LINK></LI>
+<LI><LINK to="https://drive.google.com/file/d/11ted3Gn1YWr_QSKBZoxLCjQHHq5Qh-6K/view?usp=sharing">Lecture 12</LINK></LI>
+<LI><LINK to="https://www.youtube.com/watch?v=0aX52znHbJo&t=39s">Lecture 13</LINK></LI>
+<LI><LINK to="https://www.youtube.com/watch?v=6jEwtWR3tIQ&t=251s">Lecture 14</LINK></LI>
+<LI><LINK to="https://www.youtube.com/watch?v=B3-EEmYAJK8&t=5s">Lecture 15</LINK></LI>
+<LI><LINK to="https://www.youtube.com/watch?v=aOIx7rXKT4s">Lecture
+16</LINK></LI>
+<LI><ALERT/><LINK to="ex1.html">Exercise set 1</LINK></LI>
+</UL>
 <COMMENT>
 <HEAD2>Class notes</HEAD2>
 I shall be updating the class notes during the course. 
