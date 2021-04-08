57f9eb6b2d616aca409e946b539e1189b44f77e2
covid1974 <arnabc74@gmail.com>
2021-04-08 21:20:01 +0530

    silly

--- complex/index.rb
+++ complex/index.rb
@@ -7,11 +7,14 @@ baiTAte @{<E>Moebius transform</E>@}-er ekTA viDior ul+lekh Achhe. seTA <LINK to
 <HEAD1>bhultruTi</HEAD1>
 <UL>
 <LI>
-<CIMG web="p14.png">@{<E>14</E>@}  nambar pAtAr a.nsha</CIMG>
+<IMG web="p14.png">@{<E>14</E>@}  nambar pAtAr a.nsha</IMG>
 ekhAne @{<E>equality</E>@}-r shar+taTA bhul diye felechhi. shar+taTA habe @{<E>all the <M>z_i</M>'s have a common argument,</E>@}  
 arthA.t shudhu ye tArA sakalei kono @{<E>origin</E>@}-gAmI ekai lAine thAkbe tAi nay, tArA AbAr @{<E>origin</E>@}-er ekai 
 pAshe thAkbe. nIcher chhabiTA dekhe nAo. 
-<CIMG web="p14expl.png">@{<E>(a)</E>@}  chalbe nA, @{<E>(b)</E>@}  chalbe.</CIMG>
+<IMG web="p14expl.png">@{<E>(a)</E>@}  chalbe nA, @{<E>(b)</E>@}  chalbe.</IMG>
+</LI>
+<LI><IMG web="p92.png">@{<E>92</E>@}  pAtAr a.nsha</IMG>
+lAl diye gherA a.nshaTA habe @{<M>aw+b = -[[ac][d]]+b = -[[ac-bd][c]].</M>@}  bhulTA dharechhe praNay manDal. 
 </LI>
 </UL>
 <DISQUSB url="http://www.isical.ac.in/~arnabc/complex/index.html" id="complex"></DISQUSB>
---
+++ numana2021/nr.r
@@ -0,0 +1,3 @@
+x=1
+(x = cos(x))
+(x = (x*sin(x)+cos(x))/(1+sin(x)))
