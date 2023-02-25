4ca092ad19eaa8d94f37338a8b4002c4e75f9a50
Arnab Chakraborty <arnabc74@gmail.com>
2023-02-25 13:26:46 +0530

    mult

---
+++ mult/index.rb
@@ -0,0 +1,8 @@
+<NOTE>
+@{<HEAD1>Multivariable Calculus</HEAD1>@}
+eiTA AmAr @{<E>Multivariable Calculus</E>@}  baiyer oyebpej. bhultruTi yA berobe, ekhAne tAr
+ tAlikA thAkbe. baiyer yekhAne @{<E>functional independence</E>@}-er AlochanA chhila, sekhAne ekTA
+ pepArer kathA balechhilAm. seTA <LINK to="WFNewns.pdf">ekhAn theke DAunloD karte pAro</LINK>.  
+<P/>
+<DISQUSB url="https://arnabc74.github.io/mult/index.html" id="mult"/>
+</NOTE> 
---
+++ mult/proof.rb
@@ -0,0 +1,5 @@
+<NOTE>
+@{<HEAD1>Chapter 1, Day 2</HEAD1>@}
+<UT u="MMBkNmnGtuA">chan</UT>
+kI kare pramAN likhte hay.
+</NOTE>
