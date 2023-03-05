1bea0909be3255c6b189e72668d657bedde3a1e7
Arnab Chakraborty <arnabc74@gmail.com>
2023-03-05 19:52:55 +0530

    riemvid

--- mult/riem.rb
+++ mult/riem.rb
@@ -1,5 +1,4 @@
 <NOTE>
 @{<HEAD1>Chapter 5, Day 29</HEAD1>@}
-<UT u="MMBkNmnGtuA">chan</UT>
-kono @{<M>f:\rr^2\to\rr</M>@}-er belAy @{<E>Riemann sum</E>@}-er @{<E>convergence.</E>@}  
+<UT u="sE_R7xfvK8E">chan</UT>
 </NOTE>
