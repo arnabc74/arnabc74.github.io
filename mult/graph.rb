a9a2a69f28d9f3796f24601d073b37f05543fccf
Arnab Chakraborty <arnabc74@gmail.com>
2023-04-03 10:03:27 +0530

    kumor

--- mult/graph.rb
+++ mult/graph.rb
@@ -2,5 +2,8 @@
 @{<HEAD1>Chapter 1, Day 1</HEAD1>@}
 @{<UT u="y37ubwA8-WE">Constant, extrusion, revolution.</UT>@}
 kichhu @{<M>f:\rr^2\to\rr</M>@}   yAder grAf sahaje kalpanA karA yAy. 
+<P/>
+kumorer chAker viDioTA <LINK to="https://www.youtube.com/watch?v=P8styuac15I">ekhAne</LINK>. 
+
 <DISQUSB url="https://arnabc74.github.io/mult/graph.html" id="graph"/>
 </NOTE>
