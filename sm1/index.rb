abc050155a1a56e6ba06f7c74d87ab77ec926964
Arnab Chakraborty <arnabc74@gmail.com>
2023-08-10 13:13:32 +0530

    classlink

--- sm1/index.rb
+++ sm1/index.rb
@@ -5,8 +5,7 @@ B.Stat(hons) first year students (2023). I shall post various
 things (code snippets, data sets etc) here from time to time.
 
 <P/>
-<LINK to="https://classroom.google.com/c/NjE3NzIwOTMyMjA5?cjc=nv4afuy">Google
-classroom joining link</LINK>
+Please join Google classroom with <LINK to="https://classroom.google.com/c/NjE3NzIwOTMyMjA5?cjc=nv4afuy">this joining link</LINK>
 <P/>
 <NEWS src="news.html"/>
 <P/>
