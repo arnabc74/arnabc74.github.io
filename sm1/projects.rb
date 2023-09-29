2e631febfbe0a806b61e82bf0cc8f5c6c3b5dd68
Arnab Chakraborty <arnabc74@gmail.com>
2023-09-29 15:17:52 +0530

    projstudents

--- sm1/projects.rb
+++ sm1/projects.rb
@@ -23,23 +23,66 @@ it is popularly called. Your task will be to work with still
 photos with a green (or whatever colour you wish) background,
 and to remove it keeping the foreground as intact as
 possible. The photos used MUST be taken by the project group
-(not downloaded). </LI>
+(not downloaded). 
+<BOX name="Students"> Akanksha Das(BS2307)
+                                                            Sohan Karmakar(BS2349)
+                                                             Unnati Mandal(BS2355)
+                                                            Soumya
+                                                            Chanda(BS2350)</BOX>
+
+<BOX name="Students">
+Srijeet Bhattacharjee(BS2357),Aishee Bhattacharya(BS2306),Bipayan Banerjee(BS2323),Soham Chatterjee(BS2346) and Rahul Konar(BS2338) </BOX>
+</LI>
 <LI><B>Denoising audio:</B> Record yourself talking in a room
 with mild background noise (e.g., with an electric fan or noisy
 AC on). Ocassionally pause during talking. Your job is to
 automatically process the recorded to audio to identify these
-pauses, and to make the pauses absolutely silent.</LI>
+pauses, and to make the pauses absolutely silent.
+<BOX name="Students">
+Pratyush Patra (BS2336),
+Abhirup Mistry (BS2302),
+Nandita Sen (BS2333),
+Soham Dutta (BS2347),
+Rwitobroto Dey (BS2341),
+Aryama Ghosh (BS2316)
+</BOX>
+
+<BOX name="Students">
+Adeesh Devasthale (BS2303)
+Aditya Aryan (BS2305)
+Deep Hariya (BS2324)
+Nishant Lamboria (BS2334)
+Sauparna Kar (BS2343)
+Srijan Bhowmic (BS2352)
+</BOX>
+
+<BOX name="Students">
+Kirit Maitra (BS2329)
+Mayank Mitra (BS2331)
+Vinayak Tripathi (BS2356)
+Soham Kabir (BS2348)
+Supratim Mondal (BS2354)
+</BOX>
+</LI>
 <LI><B>Classifying iris flowers:</B> Come up with the ``best
 possible'' classifier that will classify the iris flowers using
 Anderson's iris data. Coming up with a reasonable definition of
-``best possible'' is a major part of the challenge.</LI>
+``best possible'' is a major part of the challenge.
+<BOX name="Students"></BOX>
+</LI>
 <LI><B>Missile launch planning:</B> This impressive project is
 somewhat marred by the fact that the missile involved is just a rubber
 band. The launcher consists of a ruler along which the band is
 pulled to some known extent before being released. Your job is
 to collect data and come up with a formula that tells you how
 much to stretch in order to reach a given distance. You must
-also give some error bound.</LI>
+also give some error bound.
+<BOX name="Students">
+Anjan sadhukhan (BS2310)
+Ayush Baran Sen  (BS2322)
+Rahul Ganguly (BS2337)
+Rupayan Chowdhury (BS2340)</BOX>
+</LI>
 <LI><B>Distance estimation:</B> This one will require an app
 that you need to download from. The app records the ambient audio
 for a fixed period (say 2 secs), and then shows the standard
@@ -47,20 +90,29 @@ deviation of the values. Hopefully that is the loudness of the
 sound. Now stand at different known distances from a noise source, and
 see the value shown on the app. Your job is to come up with a
 formula to estimate the distance from any given app reading. You
-must provide some suitable error bar.</LI>
+must provide some suitable error bar.
+<BOX name="Students"></BOX>
+</LI>
 <LI><B>Skew angle estimation:</B> Any QR code or bar code reader has to
 do a skew angle estimation, i.e., estimate the angle at which the object is
 tilted w.r.t. the camera. Take a page of parallel lines, and
 photograph them from  different angles. Try to come up with an
-estimation technique. Check by rotating the image by that angle.</LI>
+estimation technique. Check by rotating the image by that angle.
+<BOX name="Students">Arikith, Tamim  Abhik </BOX>
+
+
+</LI>
 <LI><B>Develop an online casino:</B> Learn about at least 5
 different gambling schemes used in real casinos, and implement
 them using simulation. There should be an option to compute the
-long term average and standard deviation for each of them.  </LI>
+long term average and standard deviation for each of them.  
+</LI>
 <LI><B>Rifle shuffle:</B> Simulate riffle shuffle using the
 model done in class. Then plot the probabilities of various
 events as the number of shuffles increase to see how many
-shuffles are needed to reeach complete mixing.</LI>
+shuffles are needed to reeach complete mixing.
+<BOX name="Students"></BOX>
+</LI>
 <LI><B>Shannon's mind reading game:</B> Claude Shannon, the
 father of informaton theory, is credited with inventing this
 magic trick. An ordered deck is rifle shuffled thrice. A card is
@@ -69,7 +121,9 @@ handed back to the magician, who finds the card. The trick is
 that there will be just one singleton rising sequence with a
 ``high probability''. This singleton must be the chosen
 card. Use simulation to find the probability of success of this
-trick.</LI>
+trick.
+<BOX name="Students"></BOX>
+</LI>
 <LI><B>Separating a mixture:</B>
 Consider this random experiment: toss a fair coin, if head, then
 generate from <M>N(\mu_1,\sigma^2_1)</M> else generate
@@ -77,6 +131,16 @@ from <M>N(\mu_2,\sigma^2_2).</M>
 The data consist of IID outcomes of this random experiment. Devise some way to estimate 
 <M>\mu_1,\mu_2,\sigma^2_1</M> and <M>\sigma^2_2</M> from the
 data. Implement in R. 
+<BOX name="Students"></BOX>
+</LI>
+<LI><B>Applying discrete arc sine law for sojourn times on various sports</B>
+<BOX name="Students">
+Anunoy Chal BS2313
+Aryan Sahu BS2317
+Aryanil Kundu BS2318
+Atmadeep Sengupta BS2320
+Adity Banerjee (BS2304)
+</BOX>
 </LI>
 </OL>
 </NOTE>@}
