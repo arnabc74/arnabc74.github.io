f8912cfc8e2c73a340e9213b6014f001cac56544
Arnab Chakraborty <arnabc74@gmail.com>
2023-10-05 10:31:31 +0530

    projass

--- sm1/projects.rb
+++ sm1/projects.rb
@@ -8,6 +8,11 @@ may use R or Python or Java or C or C++. No project specific
 library is allowed.
 
 <P/>
+<ALERT/><B>The deadline for submission of project is the last
+day before the semestral exam week starts. Submit report (problem
+description, algo, findings etc) and code. Do not include code
+in your report.</B>
+<P/>
 <ALERT/>The projects are often tough, while your expertise in
 statistics is still rudimentary. So do not despair if your
 results are not satisfactory. Your performance will be judged in
@@ -31,7 +36,18 @@ possible. The photos used MUST be taken by the project group
                                                             Chanda(BS2350)</BOX>
 
 <BOX name="Students">
-Srijeet Bhattacharjee(BS2357),Aishee Bhattacharya(BS2306),Bipayan Banerjee(BS2323),Soham Chatterjee(BS2346) and Rahul Konar(BS2338) </BOX>
+Srijeet Bhattacharjee(BS2357),Aishee Bhattacharya(BS2306),Bipayan
+Banerjee(BS2323),Soham Chatterjee(BS2346) and Rahul
+Konar(BS2338) </BOX>
+
+<BOX name="Students"> 
+ Aman Singh (BS2308),  
+ Satyaprakash Sahdev Jaiswar (BS2342),
+ Ronak Gupta (BS2339),
+ Saurabh Bhikan Nankar (BS2344), 
+ Subhadip Roy (BS2353),
+ Pallav Sahay (BS2335)
+</BOX>
 </LI>
 <LI><B>Denoising audio:</B> Record yourself talking in a room
 with mild background noise (e.g., with an electric fan or noisy
@@ -62,13 +78,16 @@ Mayank Mitra (BS2331)
 Vinayak Tripathi (BS2356)
 Soham Kabir (BS2348)
 Supratim Mondal (BS2354)
+Harshit Kalotra (BS2326)
 </BOX>
 </LI>
 <LI><B>Classifying iris flowers:</B> Come up with the ``best
 possible'' classifier that will classify the iris flowers using
 Anderson's iris data. Coming up with a reasonable definition of
 ``best possible'' is a major part of the challenge.
-<BOX name="Students"></BOX>
+<BOX name="Student">
+Krishnam Baregama (BS2330)
+</BOX>
 </LI>
 <LI><B>Missile launch planning:</B> This impressive project is
 somewhat marred by the fact that the missile involved is just a rubber
@@ -84,14 +103,19 @@ Rahul Ganguly (BS2337)
 Rupayan Chowdhury (BS2340)</BOX>
 </LI>
 <LI><B>Distance estimation:</B> This one will require an app
-that you need to download from. The app records the ambient audio
+that you need to download from <LINK to="sounddata.apk">here</LINK>. The app records the ambient audio
 for a fixed period (say 2 secs), and then shows the standard
 deviation of the values. Hopefully that is the loudness of the
 sound. Now stand at different known distances from a noise source, and
 see the value shown on the app. Your job is to come up with a
 formula to estimate the distance from any given app reading. You
 must provide some suitable error bar.
-<BOX name="Students"></BOX>
+<BOX name="Students">
+Aishik Bhattacharya (BS2358),
+Arnab Palit (BS2315),
+Inesh Chattopadhyay (BS2328),
+Sourodeep Kanjilal (BS2351)
+</BOX>
 </LI>
 <LI><B>Skew angle estimation:</B> Any QR code or bar code reader has to
 do a skew angle estimation, i.e., estimate the angle at which the object is
@@ -122,7 +146,10 @@ that there will be just one singleton rising sequence with a
 ``high probability''. This singleton must be the chosen
 card. Use simulation to find the probability of success of this
 trick.
-<BOX name="Students"></BOX>
+<BOX name="Students">
+Himadri Mandal (BS2327), Ayan Ghosh (BS2321), Drishti Singla (BS2325), Mrittika
+Giri (BS2332), Aman Verma(BS2309), Siddhartha Bhattacharya(BS2345)
+</BOX>
 </LI>
 <LI><B>Separating a mixture:</B>
 Consider this random experiment: toss a fair coin, if head, then
@@ -140,7 +167,7 @@ Aryan Sahu BS2317
 Aryanil Kundu BS2318
 Atmadeep Sengupta BS2320
 Adity Banerjee (BS2304)
-</BOX>
+Ankit Bhar (BS2311)</BOX>
 </LI>
 </OL>
 </NOTE>@}
