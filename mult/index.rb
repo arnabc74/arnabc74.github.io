575a8c71def135e4c8cc9f5ef8b5cbac91e34abb
Arnab Chakraborty <arnabc74@gmail.com>
2023-11-09 11:36:54 +0530

    pratap

--- mult/index.rb
+++ mult/index.rb
@@ -5,18 +5,23 @@ eiTA AmAr @{<E>Multivariable Calculus</E>@}  baiyer oyebpej. bhultruTi yA berobe
  pepArer kathA balechhilAm. seTA <LINK to="WFNewns.pdf">ekhAn theke DAunloD karte pAro</LINK>.  
 <HEAD2>bhul-truTi</HEAD2>  
 <UL>
+<LI><IMG web="p40.png">@{<E>40</E>@}  pAtAr a.nsha.</IMG>
+ei pAtAy ekrAsh bhul kare felechhi. Asale @{<M>0</M>@}  Ar @{<M>1</M>@}-er madhye guliye felechhilAm lekhAr samaye, fale 
+bibhinna a.nsher sAmanjasya naSTa haye gechhe. 
+bhulTA dharechhen pratAp manDal. .  
+</LI>
 <LI><IMG web="p98.png">@{<E>98</E>@}  pAtAr a.nsha.</IMG>
 ekhAne oi lAl a.nshagulo bhul kare bAd diye felechhilAm.
 bhulTA dharechhen abantikA rAy barmaN. 
 </LI>
 <LI><IMG web="p152a.png">@{<E>152</E>@}  pAtAr a.nsha.</IMG>
 trritIya adhyAyer ei pAtAy Ami @{<E>copy-paste</E>@}  karte giye bhul kare felechhi. lAl ra.n diye bhul eba.n sa.nshodhan 
-duii dekhAlAm. bhulTA dharechhe abantikA rAy barmaN. 
+duii dekhAlAm. bhulTA dharechhen abantikA rAy barmaN. 
 </LI>
 <LI><IMG web="p152.png">@{<E>152</E>@}  pAtAr Arek a.nsha.</IMG>
 trritIya adhyAyer sei ekai pAtAy Ami @{<E>copy-paste</E>@}  karte giye AbAr bhul kare felechhi. lAl ra.n
  diye bhul eba.n sa.nshodhan 
-duii dekhAlAm. e bhulTAo dharechhe abantikA rAy barmaN. 
+duii dekhAlAm. e bhulTAo dharechhen abantikA rAy barmaN. 
 </LI>
 <LI><IMG web="p268.png">@{<E>268</E>@}  pAtAr a.nsha.</IMG>
 ekhAne oi lAl diye dekhAno
@@ -26,21 +31,21 @@ ekhAne oi lAl diye dekhAno
 <LI><IMG web="p234.png">@{<E>234</E>@}  pAtAr a.nsha.</IMG>
 eTA Asale @{<E>170</E>@}  nambar pAtAr @{<E>Exercise 5</E>@}-er uttar. ekhAne oi lAl @{<M>x^2</M>@}-TAr Age ekTA mAinAs 
 basbe. 
-bhulTA dharechhe abantikA rAy barmaN. 
+bhulTA dharechhen abantikA rAy barmaN. 
 </LI>
 <LI><IMG web="p267.png">@{<E>267</E>@}  pAtAr a.nsha.</IMG>
 ei parIxAr a.nkaTA bhul TukechhilAm. bhul eba.n sa.nshodhan lAl
  diye dekhAlAm. 
-bhulTA dharechhe abantikA rAy barmaN. 
+bhulTA dharechhen abantikA rAy barmaN. 
 </LI>
 <LI><IMG web="p270.png">@{<E>270</E>@}  pAtAr a.nsha.</IMG>
 eTA Asale @{<E>241</E>@}  nambar pAtAr @{<E>Exercise 2</E>@}-er uttar. bhul eba.n sa.nshodhan duii lAl
  diye dekhiyechhi. 
-bhulTA dharechhe abantikA rAy barmaN. 
+bhulTA dharechhen abantikA rAy barmaN. 
 </LI>
 <LI>pancham adhyAye @{<E>266 </E>@} nambar pAtAr @{<E>Exercise 21</E>@}-er uttar habe
  @{<E>(D).</E>@}  bhul kare @{<E>(B)</E>@}  likhe felechhi.
-bhulTA dharechhe abantikA rAy barmaN. </LI>
+bhulTA dharechhen abantikA rAy barmaN. </LI>
 <LI><IMG web="p294.png">@{<E>294</E>@}  pAtAr a.nsha.</IMG>
 ekhAne oi lAl diye dekhAno
  @{<E>integral</E>@}-TA habe @{<M>\pi \int_{-1}^2 x^4\, dx.</M>@}  tAi uttarTA habe @{<M>[[33\pi][5]].</M>@}
