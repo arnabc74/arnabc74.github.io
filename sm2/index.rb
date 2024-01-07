64d263f2ae7277a3cd5a0617395cb7ecf954c227
Arnab Chakraborty <arnabc74@gmail.com>
2024-01-07 07:42:32 +0530

    sm2start

---
+++ sm2/index.rb
@@ -0,0 +1,51 @@
+@{<NOTE>
+<HEAD1>Statistical Methods 2 (2024)</HEAD1>
+This is the course webpage for Statistical Methods 2 for
+B.Stat(hons) first year students (2023-24). I shall post various
+things (code snippets, data sets etc) here from time to time.
+
+<P/>
+<ALERT/>Please see the <LINK to="projects.html">projects page</LINK>.
+
+Google classroom <LINK to="https://classroom.google.com/c/NjUxMjAxMDM3Njk0?cjc=n3nmi54">joining link</LINK>
+<P/>
+<NEWS src="news.html"/>
+<P/>
+
+<HEAD2>Brief lecture notes</HEAD2>
+
+<HEAD2>Reference materials</HEAD2>
+Our main textbooks are the following two excellent texts.
+<DL>
+<DT><B>Mathematical statistics with applications</B>
+by <I>Ramachandran and Tsokos</I></DT>
+<DD>We shall need only chapters 4,5,6 and 7.</DD>
+
+<DT><B>Introduction to statistical theory</B> by <I>Hoel, Port
+and Stone</I></DT>
+<DD>We shall need only chapter 1, 2 and 3.</DD>
+</DL>
+These texts, however, do not cover multiple and partial
+correlations adequately. So we need the following:
+<DL>
+<DT><LINK to="pandmcorr.pdf">A few (poorly) scanned pages from
+some(?) book</LINK></DT><DD>A bunch of standard formulae that
+you MUST NOT cram!</DD>
+<DT>Some videos on (adjusted) <M>R^2</M>: 
+[
+<LINK to="https://youtu.be/krTAqkcpAvY">1</LINK>|
+<LINK to="https://youtu.be/M-BY2mVV2Rg">2</LINK>|
+<LINK to="https://youtu.be/oFXJcjOg2JU">3</LINK>|
+<LINK to="https://youtu.be/NHST-e6kBzU">4</LINK>
+]
+</DT>
+<DD>I have borrowed these from my Linear Models course. Some
+students have kindly provided the transcripts for these:
+[<LINK to="v85.pdf">1</LINK>|<LINK to="v86.pdf">2</LINK>|NA|<LINK to="v88.pdf">4</LINK>]
+
+</DD>
+</DL> 
+We shall
+use the R environment for all our computation. 
+
+</NOTE>@}
