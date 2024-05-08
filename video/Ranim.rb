5423bcef25cf3a10632563717d0869b96912a4e8
Arnab Chakraborty <arnabc74@gmail.com>
2024-05-08 23:05:54 +0530

    ranim2

--- video/Ranim.rb
+++ video/Ranim.rb
@@ -3,4 +3,11 @@
 All the functions are provided in
 this <LINK to="parAnim.r">single R file</LINK>. The example in it
 uses <LINK to="vase.png">this image</LINK>.
+
+The code is written in a somewhat dense
+way. 
+<LINK to="https://www.youtube.com/playlist?list=PLx--gUxue3Z9uTGygFZKHXomtdURZhmlS">
+This Youtube playlist</LINK>
+explains the concepts underlying the code.
+
 </NOTE>@}
