49911d7a90f2e1f89fd21b3773b7d7cf50b66a27
Arnab Chakraborty <arnabc74@gmail.com>
2024-07-29 06:01:52 +0530

    jga

--- index.rb
+++ index.rb
@@ -1,5 +1,9 @@
 @{<NOTE>
-<HEAD1>Arnab Chakraborty's Umpteenth Home</HEAD1>
+<HEAD1>Arnab Chakraborty's home page</HEAD1>
+Thanks for visiting my page. 
 
+You may like to
+visit <LINK to="https://youtube.com/@arnabc?si=YeAihbJQBcOyMqxX">my Youtube channel</LINK>. 
 
+I like to write math books in Bengali. 
 </NOTE>@}
--- jsoft/ga.rb
+++ jsoft/ga.rb
@@ -50,32 +50,44 @@ symbolically by <M>\langle v\rangle_k.</M> In J
 
 <PRE>
 cin=: 4 :0"0 2
- vy=:{:"1 y
- cy=:;{."1 y
- wh=:x=vy
- if. +./ wh do.
-    wh#cy
- else. 
-    0
+ vy=:{:"1 y  NB. Extract the array of e's.
+ cy=:;{."1 y NB. Extract the array of coefficients
+ wh=:x=vy    NB. Find which terms have the matching e's.
+ if. +./ wh do. NB. If there is at least one nonzero 
+    wh#cy       NB.   return the corresponding coefficients.
+ else.         
+    0          
  end.
 )
-vp=: |:@:(((,1);(,2);,3)&cin )
-sp=: (<i.0)&cin
+ 
+vp=: |:@:(((,1);(,2);,3)&cin ) NB. Extract the 1-vector part. The |: makes a list.
+sp=: {.@((<i.0)&cin)  NB. Extract the scalar part. The {. makes the result an atom.
 
-f=: 4 : 0
-  left=: (x>y)#y
-  xniny=:1-x e. y
-  mid =: xniny#x
-  (2|#left){ 1 _1
+NB. The next function is not used.
+oldf=: 4 : 0
+  left=: (x>y)#y          NB. How many y-elements are smaller than corresponding x-elements.
+  NB. xniny=:1-x e. y
+  NB. mid =: xniny#x
+  (2|#left){ 1 _1         NB. The answer is the parity of this.
 )
 
-g=: 4 : 0
-  s1=:*/x f"0 1 y
-  s2=: (5 e. x)*. 5 e. y
-  sign=:s1*s2{1 _1
-  sign;/:~ x (-.,-.~) y
+NB. Suppose that you have a sorted list 1 2 3 8 9 and you want to insert 3 in it from left.
+NB. What is the *smallest* number of elements in the list that it has to cross? The answer is
+NB. 2 (it needs to cross 1 and 2). The following function returns +1 if this is even, -1 else. 
+f=:{{ 1 _1 {~ 2|+/x>y  }}
+
+NB. The following verb find products of basis vectors.
+NB. Each basis vector is presented as a list of indices.
+NB. It finds the symmetric difference, sorts it, and appends a sign before.
+g=: 4 : 0                     
+  s1=:*/x f"0 1 y        NB. -1 if odd number of swaps needed to merge x and y
+  s2=: 1 _1 {~ (5 e. x)*. 5 e. y NB. -1 iff both x and y contain 5
+  sign=:s1*s2            
+  sign;/:~ x (-.,-.~) y NB. sign followed by the sorted symmetric difference of x and y
 )
 
+NB. The main work horse behind the geometric product. Its multiplies just two terms.
+NB. e.g. (2; 1 3) mult (3; 2 3) should give (_6; 1 2)
 mult=: 4 : 0
   cx=:>{.x
   cy=:>{.y
@@ -87,8 +99,31 @@ mult=: 4 : 0
   (cx*cy*cbp);vbp
 )
 
+NB. The derived products.
+omult=: 4 :0
+tmp=:(gr x)+gr y
+tmp gx x mult y
+)
 
+lmult=: 4 :0
+tmp=:(gr y)-gr x
+tmp gx x mult y
+)
 
+rmult=: 4 :0
+tmp=:(gr x)-gr y
+tmp gx x mult y
+)
+
+dmult=: 4 :0
+tmp=:|(gr x)-gr y
+tmp gx x mult y
+)
+
+NB. Prettification is done in two steps:
+NB.       1. combining terms by basis elements,
+NB.       2. sorting by grades.
+NB. The first duty is carried out by the following verb.
 comb=: 3 : 0
   vy=:{:"1 y
   cy=:{."1 y 
@@ -101,35 +136,30 @@ comb=: 3 : 0
   nc1,. nv1
 )
 
+NB. Grade sorting is done by the following verb.
+NB. Extract the boxed list of indices,
+NB. and for each term unbox, and find
+NB. the length of the list. Finally, sort y accordingly.
+srt=:{{ y /: (#@>)"1{:"1 y }}
+
+pretty=:srt@comb
+
 gpbad=: 4 : 'comb ,/ x mult"1/ y'
+
+NB. The prettified versions that are actually used.
 gp=:comb@,/@(mult"1/)
-omult=: 4 :0
-tmp=:(gr x)+gr y
-tmp gx x mult y
-)
-lmult=: 4 :0
-tmp=:(gr y)-gr x
-tmp gx x mult y
-)
-rmult=: 4 :0
-tmp=:(gr x)-gr y
-tmp gx x mult y
-)
-dmult=: 4 :0
-tmp=:|(gr x)-gr y
-tmp gx x mult y
-)
 
 
-dp=:comb@,/@(dmult"1/)
+dp=:pretty@,/@(dmult"1/)
 
-op=:comb@,/@(omult"1/)
-lp=:comb@,/@(lmult"1/)
-rp=:comb@,/@(rmult"1/)
+op=:pretty@,/@(omult"1/)
+lp=:pretty@,/@(lmult"1/)
+rp=:pretty@,/@(rmult"1/)
 
 ip=: 0 & gx @ gp
 
-ad=:comb@,
+ad=:pretty@,
+
 sm=: 4 : 0"0 2
   tmp=: <"0 x*;{.|:y
   tmp,.{:|:y
@@ -155,17 +185,26 @@ ci=: 3 :0
   (vp y) % scale
 )
 
+NB. Reciprocal that gives correct answer only for multivectors with non-null v gp rev v 
+recip=:%@sp@(gp rev) sm rev
+
+rev=: 3 :0"1
+'a b'=.y
+newa=.a*1-2*2 3 e.~ 4|#b
+newa;b
+)
 v=: (3.4;1 3),(6.5; 1 4 5),: (_4; 1 3)
 e1=:1 2$1;1#1
 e2=:1 2$1;1#2
 e3=:1 2$1;1#3
 e4=:1 2$1;1#4
 e5=:1 2$1;1#5
-o=: 0.5 sm e4 ad e5
-i=: e5 sb e4
+o=:  e4 ad e5
+i=: 0.5 sm e5 sb e4
 
 scr=: 4 :'(c x) sb (0.5**:y) sm i'
 scp=: 4 :'(c y) dp (c x) op  i'
+
 
 9!:7'+++++++++|-'
 </PRE>
