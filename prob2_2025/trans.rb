82f5a4d7043e7263b10fd00bb4413953197f9ebf
Arnab Chakraborty <arnabc74@gmail.com>
2024-11-12 11:21:30 +0530

    prob2

---
+++ prob2_2025/trans.rb
@@ -0,0 +1,11 @@
+ @{<NOTE>
+<HEAD1>Transformations</HEAD1>
+We often work with functions of random variables. New random variables are created out of existing ones via functions. 
+So a natural requirement is to be able to work out the distributions of the new random variables in terms those of the existing
+ ones. There are quite a few techniques to do this. 
+
+<HEAD2>Via CDF</HEAD2>
+<HEAD2>Using Jacobian</HEAD2>
+<HEAD2>Sum</HEAD2>
+<HEAD2>Quotient</HEAD2>
+</NOTE>@}
