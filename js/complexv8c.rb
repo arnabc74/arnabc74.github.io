7516d6cf666d88887441c71aeb53e4cc82ec03bd
Arnab Chakraborty <arnabc74@gmail.com>
2026-04-15 21:41:55 +0530

    "complexv8"

---
+++ js/complexv8c.rb
@@ -0,0 +1,107 @@
+<MCQ>
+<QN m="10">
+<BODY>ekTA dwighAt samIkaraN Achhe @{<M>x^2+bx+c=0,</M>@}  yekhAne @{<M>b,c\in\rr.</M>@}  balA
+ Achhe @{<M>i+2</M>@}  er ekTA @{<E>root.</E>@} tabe @{<M>b,c=?</M>@}</BODY>
+<C>@{<M>b=-4,</M>@}  @{<M>c=5.</M>@}</C>
+<C>@{<M>b=4,</M>@}  @{<M>c=5.</M>@}</C>
+<C>@{<M>b=5,</M>@}  @{<M>c=-4.</M>@}</C>
+<C>balA sambhab nay.</C>
+<SOLN>
+yehetu @{<M>i+2</M>@}  hala @{<E>non-real</E>@}, tAi anya @{<E>root</E>@}-TA habe tAr
+ @{<E>conjugate</E>@}, mAne 
+@{<M>-i+2.</M>@} sAb~dhAn, @{<M>i-2</M>@}  nay kintu (@{<E>imaginary</E>@}  
+a.nsher chihna ol+TAte hay). ederke yog karle @{<M>4</M>@}  Ar guN karle @{<M>5</M>@}  hay. tAi @{<M>-[[ba]] = 4</M>@}  
+Ar @{<M>[[ca]]= 5</M>@}  habe. kintu ekhAne @{<M>a=1.</M>@}  
+</SOLN>
+</QN>
+
+<QN m="10">
+<BODY>yadi @{<M>ax^2+x+c=0</M>@}  Ar @{<M>a,c\in\rr</M>@}  hay, Ar ei samIkaraNer ekTA uttar hay
+ @{<M>3-i</M>@}, tabe @{<M>a,c</M>@}  kI kI habe?</BODY>
+<C>@{<M>a=-[[16]]</M>@}  Ar @{<M>c=-[[53]].</M>@}</C>
+<C>@{<M>a=[[16]]</M>@}  Ar @{<M>c=-[[53]].</M>@}</C>
+<C>@{<M>a=[[16]]</M>@}  Ar @{<M>c=[[53]].</M>@}</C>
+<C>@{<M>a=-[[16]]</M>@}  Ar @{<M>c=[[53]].</M>@}</C>
+<SOLN>
+duibhAbe karA yAy. <ul><li>ek hachchhe sarAsari @{<M>x=3-i</M>@}  basiye. tabe @{<M>a(3-i)^2+(3-i)+c= 0,</M>@}   bA @{<M>(8a+c+3)-(6a+1)i = 0+0i.</M>@}
+sutarA.n @{<E>real</E>@}  Ar @{<E>imaginary</E>@}  a.nsha AlAdA kare samAn kare pAchchhi duTo
+ samIkaraN @{<M>8a+c+3=0</M>@}  Ar @{<M>-(6a+1)=0.</M>@} tAi @{<M>a = -[[16]]</M>@}  Ar @{<M>c = -[[53]]</M>@}  hachchhe. 
+</li><li>
+dwitIya path hala anya uttarTAke @{<E>conjugate</E>@}  hisebe likhe felA. @{<M>3+i.</M>@}  tabe
+ duTo uttarer yogfal habe @{<M>6</M>@}  Ar guN fal habe @{<M>3^2+1^2 = 10.</M>@}  tAi @{<M>-[[ba]]
+ = 6</M>@}  Ar @{<M>[[ca]]=10</M>@}  hachchhe. Ar ekhAne @{<M>b=1</M>@}  deoyAi Achhe. sekhAn
+ thekeo @{<M>a,c</M>@}  beriye yAbe.</li></ul>  
+</SOLN>
+</QN>
+@{<COMMENT>
+rectform(a*(3-%i)^2+(3-%i)+c)
+</COMMENT>@}
+<QN m="15" nota="y" a="3">
+<BODY>yadi @{<M>ax^2-6x+c=0</M>@}  Ar @{<M>a,c\in\rr</M>@}  hay, Ar ei samIkaraNer ekTA @{<E>root</E>@} hay
+ @{<M>3-\sqrt2</M>@}, tabe @{<M>a,c</M>@}  kI kI habe?</BODY>
+<C>@{<M>a=1, c=7</M>@}  hate bAdhya.</C>
+<C>@{<M>a=2, c=14</M>@}  hate bAdhya.</C>
+<C>@{<M>a=7, c=1</M>@}  hate bAdhya.</C>
+<C>eTuku tathyer bhittite bAr karA asambhab.</C>
+<SOLN>
+ekhAne khAli ekTAi samIkaraN pAchchhi @{<M>a(3-\sqrt2)^2-6(3-\sqrt2)+c=0.</M>@}  e theke duTo ajAnA rAshi @{<M>a</M>@}  
+Ar @{<M>c</M>@}  kI kare bAr karba? yeman tumi yA khushi @{<M>a</M>@}  nAo, Ar @{<M>c</M>@}  nAo tAr sAthe tAl rekhe @{<M>c=6(3-\sqrt2)-a(3-\sqrt2)^2</M>@}  nilei
+ amni ei samIkaraNTA pAlita habe. fale asa.nkhya erakam @{<M>a</M>@}  Ar @{<M>c</M>@}  pAbe. 
+<P/>
+@{<E>Non-real</E>@}  uttarer xetre AmrA duTo samIkaraN pAchchhilAm, @{<E>real</E>@}  Ar @{<E>imaginary</E>@}  a.nshake AlAdA 
+kare. ekhAne se subidhA nei. 
+</SOLN>
+</QN>
+
+<QN m="10" nota="y">
+<BODY>ekTA @{<E>quadratic equation</E>@}  eirakam,  @{<M>x^2+bx+c=0</M>@}, yekhAne
+ @{<M>b,c\in\rr.</M>@}  yadi @{<M>z</M>@}  er ekTA @{<E>non-real</E>@}  @{<E>root</E>@}  hay,
+ tabe nIcher kon kathATA Thik hate bAdhya?</BODY>
+<C>@{<M>b=-2Re(z).</M>@}</C>
+<C>@{<M>b=2Re(z).</M>@}</C>
+<C>@{<M>b=Im(z).</M>@}</C>
+<C>eder konoTAi nay.</C>
+<SOLN>
+yehetu @{<M>z</M>@}  hala ekTA @{<E>non-real</E>@}  bIj, tAi @{<M>\overline{z}</M>@}  habe anyaTA. sutarA.n @{<M>-[[ba]] = z+\overline z = 2Re(z).</M>@}  
+ekhAne @{<M>a=1.</M>@}  
+</SOLN>
+</QN>
+
+<QN m="10">
+<BODY>ekTA  dwighAt samIkaraN dilAm @{<M>ax^2+bx+1=0</M>@}  yekhAne @{<M>a,b\in\rr.</M>@}  er
+ekTA @{<E>non-real root</E>@}  hala @{<M>p+iq,</M>@} yekhAne @{<M>p,q\in\rr.</M>@}      tabe nIcher kon
+ bAkyaTA satyi hate bAdhya? </BODY>
+<C>@{<M>a=[[1][p^2+q^2]]</M>@}</C>
+<C>@{<M>a=p^2+q^2</M>@}</C>
+<C>@{<M>b=p^2+q^2</M>@}</C>
+<C>@{<M>b=[[1][p^2+q^2]]</M>@}</C>
+<SOLN>
+anya @{<E>root</E>@}-TA @{<M>p-iq.</M>@}  habe. tAi @{<M>[[ca]] = (p+iq)(p-iq) = p^2+q^2.</M>@}  edike ekhAne @{<M>c=1.</M>@}  
+tAi @{<M>[[1a]] = p^2+q^2,</M>@}  sutarA.n @{<M>a = [[1][p^2+q^2]].</M>@}
+</SOLN>
+</QN>
+<QN m="20" nota="y" a="2">
+<BODY>duTo @{complex@} sa.nkhyA Achhe @{<M>z</M>@}  Ar @{<M>w,</M>@} yAder @{imaginary@} a.nsha shUnya nay.
+ eder biSaye duTo bAkya dichchhi--
+<ul><li>@{<M>z = \overline{w}.</M>@}</li>
+<li>@{<M>z+w</M>@}  eba.n @{<M>zw</M>@}  dujanei @{<E>real</E>@}  (mAne oder @{imaginary@} a.nsha shUnya). 
+</li></ul>
+tabe nIcher kon.h kathATA saThik?</BODY>
+<HINT>@{<M>z=a+bi</M>@}  eba.n @{<M>w=c+di</M>@}  likhe nAo, yekhAne @{<M>a,b,c,d\in\rr.</M>@} 
+ ebAr duTo bAkyakei @{<M>a,b,c,d</M>@}  diye lekho.</HINT>
+<C>prathamTA saThik hale dwitIyaTAo saThik hate bAdhya, kintu dwitIyaTA saThik haleo prathamTA saThik nAo hate pAre.</C>
+<C>dwitIyaTA saThik hale prathamTAo saThik hate bAdhya, kintu prathamTA saThik haleo dwitIyaTA saThik nAo hate pAre.</C>
+<C>prathamTA saThik hale dwitIyaTAo saThik hate bAdhya, eba.n dwitIyaTA saThik hale prathamTAo saThik hate bAdhya.</C>
+<C>er konoTAi nay.</C>
+<SOLN>@{<M>z=a+bi</M>@}  Ar @{<M>w=c+di</M>@}  nichchhi yekhAne @{<M>a,b,c,d\in\rr.</M>@}  tabe
+ pratham bAkyaTA balchhe @{<M>a=c</M>@}  Ar @{<M>b=-d.</M>@}  
+
+dwitIya bAkya balchhe @{<M>Im(z+w)=0,</M>@}  mAne @{<M>b+d=0</M>@}  Ar @{<M>Im(zw)=0</M>@}  mAne
+ @{<M>ad+bc=0.</M>@}  yehetu @{<M>b+d=0</M>@}  tAi @{<M>d=-b.</M>@}  sutarA.n @{<M>-ab+bc =
+ 0.</M>@}  edike balA Achhe @{<M>b\neq 0,</M>@}  tAi @{<M>a=c.</M>@}
+<P/>
+tAi duTo bAkyai ekai shar+te pariNata hchchhe @{<M>a=c</M>@}  eba.n @{<M>d=-b.</M>@}
+</SOLN>
+</QN>
+
+</MCQ>
